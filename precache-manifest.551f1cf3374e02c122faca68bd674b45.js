d677b18360dbac89cbea340693566253cde37b68
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-07-03 23:51:19 +0000

    Deploy Leonardocpn/guia-bolso-desafio to github.com/Leonardocpn/guia-bolso-desafio.git:gh-pages

---
+++ precache-manifest.551f1cf3374e02c122faca68bd674b45.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a083035e5f4586d911247b410cc99089",
+    "url": "/guia-bolso-desafio/index.html"
+  },
+  {
+    "revision": "ca334c2736779e77cd11",
+    "url": "/guia-bolso-desafio/static/css/main.ca68aa8f.chunk.css"
+  },
+  {
+    "revision": "b012305d0b39a45816d8",
+    "url": "/guia-bolso-desafio/static/js/2.74b361a0.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/guia-bolso-desafio/static/js/2.74b361a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d638854d1049f05742b9",
+    "url": "/guia-bolso-desafio/static/js/3.726aae6a.chunk.js"
+  },
+  {
+    "revision": "526af13f98e03da18585",
+    "url": "/guia-bolso-desafio/static/js/4.6355d50b.chunk.js"
+  },
+  {
+    "revision": "ca334c2736779e77cd11",
+    "url": "/guia-bolso-desafio/static/js/main.9b07bfa6.chunk.js"
+  },
+  {
+    "revision": "a42711cc066a21e9fbd1",
+    "url": "/guia-bolso-desafio/static/js/runtime-main.970e14c3.js"
+  },
+  {
+    "revision": "d8bf2be0930740ec83f5220abeffc9fe",
+    "url": "/guia-bolso-desafio/static/media/chuck-norris.d8bf2be0.jpg"
+  }
+]);
